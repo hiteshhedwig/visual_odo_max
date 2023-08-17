bceeecae1258dcd575195209b906953d684a561e
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-08-17 23:53:05 +0530

    matrix add : OK

--- roadmap/foundation/src/1_matrix_cal.py
+++ roadmap/foundation/src/1_matrix_cal.py
@@ -11,16 +11,39 @@ class MatrixOperations(object):
     matrix operations
     """
     def __init__(self, rows, cols):
-        self.matrix = np.zeros((rows,cols), dtype=np.float32)
+        self.matrix = np.random.rand(rows, cols).astype(np.float32)
+        self.rows = rows
+        self.cols = cols
 
     def get_matrix(self):
         """Return the matrix"""
         return self.matrix
     
-    def __add__(self, other):
-        pass
+    def get_shape(self):
+        return self.matrix.shape
     
+    def set_matrix(self,matrix):
+        self.matrix = matrix
+        return self.matrix
+    
+    def __add__(self, other):
+        # assert other.get_shape() == self.get_matrix()
+        if other.get_shape() == self.get_shape():
+            print("matrix check okay ", self.get_shape())
+        else:
+            raise ValueError(f"matrix check failed - {self.get_shape()} != {other.get_shape()}")
+                              
+        added_matrix = other.get_matrix() + self.get_matrix()
+        final_matrix = MatrixOperations(self.rows, self.cols)
+        final_matrix.set_matrix(added_matrix)
+        return final_matrix
 
+    
 if __name__ == '__main__':
-    mo = MatrixOperations(4,3)
-    print(mo.get_matrix())
+    mat_1 = MatrixOperations(4,3)
+    print("MAT 1 " , mat_1.get_matrix())
+    mat_2 = MatrixOperations(4,3)
+    print("MAT 2 ", mat_2.get_matrix())
+
+    mat_3 = mat_1 + mat_2   
+    print(mat_3.get_matrix())
