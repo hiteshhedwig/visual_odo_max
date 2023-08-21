7dd1314235f7983f2c39469c59e4815eb600be79
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-08-21 23:32:47 +0530

    update plots for covarience matrix

--- roadmap/foundation/src/4_gaussian_simulator.py
+++ roadmap/foundation/src/4_gaussian_simulator.py
@@ -1,41 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-import statsmodels.api as sm
+from scipy.stats import multivariate_normal
 
-def get_sample_normaldist(mean, std, size=None):
-    """
-    The function returns a sample from a normal distribution with a given mean and standard deviation.
+# Function to plot 2D Gaussian with given mean and covariance matrix
+def plot_2d_gaussian(mean, cov_matrix):
+    # Generate grid of points
+    x, y = np.mgrid[-3:3:.01, -3:3:.01]
+    pos = np.dstack((x, y))
     
-    :param mean: The mean is the average value of the distribution. It represents the central tendency
-    of the data
-    :param std: The standard deviation of the normal distribution. It determines the spread or
-    variability of the data. A larger standard deviation means the data points are more spread out from
-    the mean
-    :param size: The size parameter determines the shape of the output array. If size is None (default),
-    a single value will be returned. If size is an integer, a 1-D array of that length will be returned.
-    If size is a tuple, an array with that shape will be returned
-    :return: a sample from a normal distribution with the specified mean and standard deviation. The
-    size parameter determines the shape of the returned array.
-    """
-    return np.random.normal(mean, std, size)
-
-
-def main():
-    data = get_sample_normaldist(0,1, size=(10000,))
-    print(data)
-    # displays gaussian distribution
-    plt.hist(data, bins=30)
+    # Evaluate the Gaussian distribution on the grid
+    rv = multivariate_normal(mean, cov_matrix)
+    plt.contourf(x, y, rv.pdf(pos), levels=50, cmap="viridis")
+    
+    # Plot settings
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.title("2D Gaussian Distribution")
+    plt.colorbar(label="Probability Density")
+    plt.axis("equal")
     plt.show()
 
-    #prob density function - PDF 
-    sns.kdeplot(data)
-    plt.show()
+# Mean vector
+mean = [0, 0]
 
-    # compare the distribution
-    sm.qqplot(data, line='45')
-    plt.show()
+# Different covariance matrices to visualize
+cov1 = [[1, 0], [0, 1]]
+cov2 = [[1, 0.8], [0.8, 1]]
+cov3 = [[1, -0.8], [-0.8, 1]]
 
+# Plot Gaussian distributions
+print("Circular (Independent Variables):")
+plot_2d_gaussian(mean, cov1)
 
-if __name__ == '__main__':
-    main()
+print("Positive Covariance (Tilted Counterclockwise):")
+plot_2d_gaussian(mean, cov2)
+
+print("Negative Covariance (Tilted Clockwise):")
+plot_2d_gaussian(mean, cov3)
