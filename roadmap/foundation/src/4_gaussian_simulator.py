b3d9fe6f4115b461a17b264030cd4f0aedbec782
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-08-22 09:49:13 +0530

    update covarience matrix visualization

--- roadmap/foundation/src/4_gaussian_simulator.py
+++ roadmap/foundation/src/4_gaussian_simulator.py
@@ -37,3 +37,28 @@ plot_2d_gaussian(mean, cov2)
 
 print("Negative Covariance (Tilted Clockwise):")
 plot_2d_gaussian(mean, cov3)
+
+
+# Fixed mean
+mean = [0, 0]
+
+# Different covariance matrices
+cov1 = [[1, 0], [0, 1]]  # Circle (equal variances, zero covariance)
+cov2 = [[2, 0], [0, 0.5]]  # Ellipse oriented along axes (different variances, zero covariance)
+cov3 = [[2, 1], [1, 2]]  # Tilted ellipse (positive covariance)
+cov4 = [[2, -1], [-1, 2]]  # Opposite tilted ellipse (negative covariance)
+
+distributions = [cov1, cov2, cov3, cov4]
+
+plt.figure(figsize=(10, 8))
+
+for i, cov in enumerate(distributions, 1):
+    x, y = np.random.multivariate_normal(mean, cov, 5000).T
+    plt.subplot(2, 2, i)
+    plt.scatter(x, y, s=2, alpha=0.6)
+    plt.xlim(-8, 8)
+    plt.ylim(-8, 8)
+    plt.title(f"Covariance Matrix:\n{cov}")
+
+plt.tight_layout()
+plt.show()
