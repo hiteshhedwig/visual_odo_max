e3d4162138d4d04995dd3956d10567ee481f91d1
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-08-26 17:37:33 +0530

    2 sample images bf matcher

--- roadmap/foundation/src/7_feature_matching.py
+++ roadmap/foundation/src/7_feature_matching.py
@@ -9,25 +9,43 @@ def files_list(directory):
     files = []
     for file in glob.glob(directory+"/*"):
         img = cv2.imread(file, cv2.COLOR_BGR2RGB)
-        # img = cv2.resize(img, (600,600))
+        img = cv2.resize(img, (600,600))
         files.append(img)
     return files
 
+def orb_keypoints(images):
+    kp_des_list = []
+    for image in images:
+        orb = cv2.ORB_create()
+        # find the keypoints with ORB
+        kp = orb.detect(image,None)
+        # compute the descriptors with ORB
+        kp, des = orb.compute(image, kp)
+        kp_des_list.append([kp, des])
+
+    return kp_des_list
+
 def main():
-    directory = "roadmap/foundation/assets/dogs"
+    directory = "roadmap/foundation/assets/jimmy"
     images = files_list(directory)
     print(f"{len(images)} Numbers of images are loaded")
 
-    orb = cv2.ORB_create()
-    # find the keypoints with ORB
-    kp = orb.detect(images[-1],None)
-    # compute the descriptors with ORB
-    kp, des = orb.compute(images[-1], kp)
+    kp_des_list = orb_keypoints(images)
 
-    print(des)
+    # Use BFMatcher (Brute Force Matcher) to find matches
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(kp_des_list[0][1], kp_des_list[1][1])
 
-    img2 = cv2.drawKeypoints(images[-1], kp, None, color=(255,0,0), flags=0)
-    plt.imshow(img2), plt.show()
+    # Sort matches based on their distances
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    # Draw the matches
+    img_matches = cv2.drawMatches(images[0], kp_des_list[0][0], images[1], kp_des_list[1][0], matches[:50], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    # Display the matched images
+    plt.imshow(img_matches)
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
